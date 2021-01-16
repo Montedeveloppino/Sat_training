a517b282e271782d61baa57b077484726422eb71
Youssef <annabiyoussef1986@gmail.com>
2021-01-16 13:02:23 +0100

    pimp

--- pyramid.rb
+++ pyramid.rb
@@ -6,19 +6,30 @@ i = 0
 j = 1
 char = "#"
 Floor.times do
-  puts " " * (Floor - i) + "#{char}" * j
+  puts " " * (Floor - i) + "#{char}" * j + " " * (Floor * 2 - i * 2) + "#{char}" * j
   i += 1
   j += 2
   sleep (0.1)
  end
 end
+def pyramid1_up
+  i = 0
+  j = 1
+  char = "#"
+  Floor.times do
+    puts " " * (Floor * 2 - i) + "#{char}" * j
+    i += 1
+    j += 2
+    sleep (0.1)
+   end
+  end
 
 def pyramid_down
 i = 2
 j = 3
 char = "#"
 (Floor - 1).times do
-  puts " " * i + "#{char}" * (Floor* 2 - j)
+  puts " " * i + "#{char}" * (Floor* 2 - j) + " " * 2 * i + "#{char}" * (Floor* 2 - j) 
   i += 1
   j += 2
   sleep (0.1)
@@ -28,6 +39,7 @@ end
 def perform
 pyramid_up
 pyramid_down
+pyramid1_up
 end
 
 perform
